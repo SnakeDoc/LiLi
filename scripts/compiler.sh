d3c36e466789fbba9630a2c188053d072c208c5c
Jason Sipula <alupis1@gmail.com>
2014-11-06 17:58:12 -0800

    cleanup and standarization of variables, quote everything

--- scripts/compiler.sh
+++ scripts/compiler.sh
@@ -6,36 +6,34 @@
 build_toolchain() {
     while IFS=',' read -ra ADDR; do
         for subpkg in "${ADDR[@]}"; do
-            pkg_dir=`find ${PACKAGES}/ -type d -name ${subpkg}`
+            pkg_dir="$(find ${PACKAGES}/ -type d -name ${subpkg})"
 
             echo "Processing package ${subpkg} from location ${pkg_dir}"
-            . ${pkg_dir}/package.mk
+            . "${pkg_dir}/package.mk"
             echo "building package: ${PKG_NAME}"
-            build_package ${PKG_NAME}
+            build_package "${PKG_NAME}"
 
         done
-    done <<< $1
+    done <<< "${1}"
 }
 
 function error() {
-    echo "Error building $1: failed on $2"
-    echo "Error Code: $3"
+    echo "Error building ${1}: failed on ${2}"
+    echo "Error Code: ${3}"
     echo -n "Build Toolchain: "
-    show_status ${FAIL}
-    exit $3
+    show_status "${FAIL}"
+    exit "${3}"
 }
 
 execute_script() {
-    echo "Executing as user: $2"
-    if ! ${COMPILER_SCRIPTS}/$2
-    then
-        error $1 $2 $?
+    if [ ! "${COMPILER_SCRIPTS}/${2}" ]; then
+        error "${1}" "${2}" "${?}"
     fi
 }
 
 build_package() {
-    local RETURN_VAL=0
-    case "$1" in
+    local RETURN_VAL="0"
+    case "${1}" in
         gcc)
             declare -a scripts=(\
                  "linux_headers.sh" \
@@ -48,44 +46,44 @@ build_package() {
                  )
 
             for SCRIPT_NAME in "${scripts[@]}"; do
-                execute_script $1 ${SCRIPT_NAME} $(logname)
+                execute_script "${1}" "${SCRIPT_NAME}"
                 echo ""
                 echo -n "${SCRIPT_NAME}"
-                show_status ${OK}
+                show_status "${OK}"
                 echo ""
             done
-            # TODO: Package compiler into tar.gz
             echo ""
             echo -n "Build Toolchain: "
-            show_status ${OK}
+            show_status "${OK}"
             ;;
         *)
-            echo "$1 is not supported!"
+            echo "${1} is not supported!"
             exit 1
     esac
 }
 
 # create base directory
-mkdir -pv ${CLFS}
+mkdir -pv "${CLFS}"
 # Make source dir
-mkdir -pv ${CLFS_SOURCES}
+mkdir -pv "${CLFS_SOURCES}"
 # create sysroot directory
-mkdir -pv ${CLFS_TOOLS}/${CLFS_TARGET}
-ln -sfv . ${CLFS_TOOLS}/${CLFS_TARGET}/usr
+mkdir -pv "${CLFS_TOOLS}/${CLFS_TARGET}"
+ln -sfv . "${CLFS_TOOLS}/${CLFS_TARGET}/usr"
 
 # toolchain
-. ${TOOLCHAIN_DEPS}/package.mk
+. "${TOOLCHAIN_DEPS}/package.mk"
 
-build_toolchain ${PKG_DEPENDS}
+build_toolchain "${PKG_DEPENDS}"
 
 # test toolchain
 echo ""
 echo "Testing toolchain..."
-${COMPILER_SCRIPTS}/test_compiler.sh
+"${COMPILER_SCRIPTS}/test_compiler.sh"
+error_on_fail "${?}"
 
 # finish script
 echo ""
 echo -n "Toolchain Build Complete and Tested: "
-show_status ${OK}
+show_status "${OK}"
 echo ""
 
--- scripts/system.sh
+++ scripts/system.sh
@@ -10,35 +10,34 @@
 build_system() {
     while IFS=',' read -ra ADDR; do
         for subpkg in "${ADDR[@]}"; do
-            pkg_dir=`find ${PACKAGES}/ -type d -name ${subpkg}`
+            pkg_dir="$(find ${PACKAGES}/ -type d -name ${subpkg})"
 
             echo "Processing package ${subpkg} from location ${pkg_dir}"
-            . ${pkg_dir}/package.mk
+            . "${pkg_dir}/package.mk"
             echo "Building package: ${PKG_NAME}"
-            build_package ${PKG_NAME}
+            build_package "${PKG_NAME}"
         done
-    done <<< $1
+    done <<< "${1}"
 }
 
 error() {
-    echo "Error building $1: failed on $2"
-    echo "Error Code: $3"
+    echo "Error building ${1}: failed on ${2}"
+    echo "Error Code: ${3}"
     echo -n "Building System: "
-    show_status ${FAIL}
-    exit $3
+    show_status "${FAIL}"
+    exit "${3}"
 }
 
 execute_script() {
-    echo "Executing: $2"
-    if ! ${SYSTEM_SCRIPTS}/$2
-    then
-        error $1 $2 $?
+    echo "Executing: ${2}"
+    if [ ! "${SYSTEM_SCRIPTS}/${2}" ]; then
+        error "${1}" "${2}" "${?}"
     fi
 }
 
 build_package() {
-    local RETURN=0
-    case "$1" in
+    local RETURN="0"
+    case "${1}" in
         system)
             declare -a scripts=(\
                 "rootfs.sh" \
@@ -57,41 +56,41 @@ build_package() {
 
             for SCRIPT_NAME in "${scripts[@]}"; do
                 setup_fakeroot
-                execute_script $1 ${SCRIPT_NAME}
-                package_fakeroot $(echo ${SCRIPT_NAME} | cut -d "." -f 1)
+                execute_script "${1}" "${SCRIPT_NAME}"
+                package_fakeroot "$(echo ${SCRIPT_NAME} | cut -d "." -f 1)"
                 echo ""
                 echo -n "${SCRIPT_NAME}"
-                show_status ${OK}
+                show_status "${OK}"
                 echo ""
             done
 
             # package into tarball
             echo ""
             echo "Packing base packages"
-            execute_script $1 "archive.sh"
+            execute_script "${1}" "archive.sh"
             echo ""
             echo -n "archive.sh"
-            show_status ${OK}
+            show_status "${OK}"
             echo ""
 
             echo ""
             echo -n "Build System: "
-            show_status ${OK}
+            show_status "${OK}"
             ;;
         *)
-            echo "$1 is not supported!"
+            echo "${1} is not supported!"
             exit 1
     esac
 }
 
 # system package
-. ${SYSTEM_DEPS}/package.mk
+. "${SYSTEM_DEPS}/package.mk"
 
-build_system ${PKG_DEPENDS}
+build_system "${PKG_DEPENDS}"
 
 # finish script
 echo ""
 echo -n "System Build Complete: "
-show_status ${OK}
+show_status "${OK}"
 echo ""
 
