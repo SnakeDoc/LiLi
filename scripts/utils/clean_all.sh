cd964ed0f4510ae84a7fad77685bed9f510bce3a
Jason Sipula <alupis1@gmail.com>
2014-12-22 22:13:42 -0800

    really clean all

--- scripts/utils/clean_all.sh
+++ scripts/utils/clean_all.sh
@@ -7,10 +7,7 @@ set -u
 
 # Clean all directories
 
-${SCRIPTS}/utils/clean_sources.sh
-${SCRIPTS}/utils/clean_compiler.sh
-${SCRIPTS}/utils/clean_system.sh
-${SCRIPTS}/utils/clean_packages.sh
+rm -rf "${CLFS}/"
 
 exit 0
 
