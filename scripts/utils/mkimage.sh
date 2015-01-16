f4bf359643fe274ffacc2c46e4e1b1dc3f16ede1
Jason Sipula <alupis1@gmail.com>
2015-01-16 00:09:13 -0800

    re-write mkimage script to be more portable

--- scripts/utils/mkimage.sh
+++ scripts/utils/mkimage.sh
@@ -8,6 +8,14 @@ set -u
 . settings/config
 . scripts/utils/utils.sh
 
+LOOP="$(losetup -f)"
+
+DISK_SIZE="$((${SYSTEM_SIZE} + ${STORAGE_SIZE} + 4))"
+IMAGE_DATE=$(date +%Y%m%d)
+SHORT_VERSION=$(echo ${VERSION} | cut -d " " -f 1)
+IMAGE_NAME="${IMAGE_DATE}-${OS_NAME,,}-${SHORT_VERSION}-${CLFS_ARM_ARCH}-rpi.img"
+DISK="${IMAGE_DIR}/${IMAGE_NAME}"
+
 if [ -e "${IMAGE_DIR}" ]; then
 
    echo "Image Directory exists! Cleaning..."
@@ -17,83 +25,92 @@ if [ -e "${IMAGE_DIR}" ]; then
 fi
 
 echo "Creating Image Directory..."
-
 mkdir -pv "${IMAGE_DIR}"
 
-IMAGE_DATE=$(date +%Y%m%d)
-SHORT_VERSION=$(echo ${VERSION} | cut -d " " -f 1)
-IMAGE_NAME="${IMAGE_DATE}-${OS_NAME,,}-${SHORT_VERSION}-${CLFS_ARM_ARCH}-rpi.img"
+echo "Creating base directory structure..."
+MNT_TMP="${IMAGE_DIR}/rootfs"
+mkdir -pv "${MNT_TMP}"
+check_status
+
+# ensure loop is not in use
+umount "${MNT_TMP}" &>/dev/null || true
+umount "${LOOP}" &>/dev/null >/dev/null || true
+losetup -d "${LOOP}" &>/dev/null >/dev/null || true
 
 echo "Creating Image File ${IMAGE_NAME} in ${IMAGE_DIR}"
 
-dd if=/dev/zero of="${IMAGE_DIR}/${IMAGE_NAME}" bs="${IMAGE_SIZE_BS}" count="${IMAGE_SIZE_COUNT}"
-check_status
-
-# we wrote a lot to disk, sync now
-echo ""
-echo -n "Sync disk"
-sync
-check_status
-
-# now make the partitions
-echo ""
-echo "Creating Image Partitions..."
-(echo n; echo p; echo 1; echo ; echo +128M; 
-   echo n; echo p; echo 2; echo ; echo ; echo t; 
-   echo 1; echo c; echo a; echo 1; echo p; echo w;) | fdisk "${IMAGE_DIR}/${IMAGE_NAME}"
-check_status
-
-# now format the partitions
-echo -n "Creating loopback devices..."
-MAPPING=($(kpartx -l "${IMAGE_DIR}/${IMAGE_NAME}" | cut -d " " -f 1))
-kpartx -a "${IMAGE_DIR}/${IMAGE_NAME}"
-check_status
-
-sleep 2
-
-echo "Formatting partition 1 as vfat..."
-mkfs.vfat "/dev/mapper/${MAPPING[0]}"
-check_status
+dd if=/dev/zero of="${DISK}" bs="${IMAGE_SIZE_BS}" count="${DISK_SIZE}"
 sync
 
-sleep 2
-
-echo "Formatting partition 2 as ext4..."
-mkfs.ext4 "/dev/mapper/${MAPPING[1]}"
-check_status
+# make disklabel
+parted -s "${LOOP}" mklabel msdos
 sync
 
-sleep 2
+# create partition 1
+echo "Creating part1 on ${DISK}"
+SYSTEM_PART_END="$((${SYSTEM_SIZE} * 1024 * 1024 / 512 + 2048))"
 
-echo "Creating base directory structure..."
-mkdir -pv "${IMAGE_DIR}/rootfs"
-check_status
+parted -s "${LOOP}" -a min unit s mkpart primary fat32 2048 "${SYSTEM_PART_END}"
+sync
+parted -s "${LOOP}" set 1 boot on
+sync
+
+# create partition 2
+echo "Creating part2 on ${DISK}"
+STORAGE_PART_START="$((${SYSTEM_PART_END} + 2048))"
+STORAGE_PART_END="$((${STORAGE_PART_START} + ((${STORAGE_SIZE} * 1024 * 1024 / 512))))"
+parted -s "${LOOP}" -a min unit s mkpart primary ext4 "${STORAGE_PART_START}" "${STORAGE_PART_END}"
+sync
+
+# create filesystem on part1
+losetup -d "${LOOP}"
+echo "Creating filesystem on part1"
+PART1_OFFSET="$((2048 * 512))"
+PART1_SIZELIMIT="$((${SYSTEM_SIZE} * 1024 * 1024))"
+losetup -o "${PART1_OFFSET}" --sizelimit "${PART1_SIZELIMIT}" "${LOOP}" "${DISK}"
+mkfs.vfat "${LOOP}"
+sync
+
+# make sure part1 is not mounted
+umount "${LOOP}" || true
+
+# create filesystem on part2
+losetup -d "${LOOP}"
+echo "Creating filesystem on part2"
+PART2_OFFSET="$((${STORAGE_PART_START} * 512))"
+PART2_SIZELIMIT="$((${STORAGE_SIZE} * 1024 * 1024))"
+losetup -o "${PART2_OFFSET}" --sizelimit "${PART2_SIZELIMIT}" "${LOOP}" "${DISK}"
+mke2fs -q -t ext4 -m 0 "${LOOP}"
+UUID_STORAGE="$(uuidgen)"
+tune2fs -U "${UUID_STORAGE}" "${LOOP}"
+e2fsck -n "${LOOP}"
+sync
 
 # now mount the image file
-echo -n "Mounting image..."
-mount "/dev/mapper/${MAPPING[1]}" "${IMAGE_DIR}/rootfs" && 
-   mkdir -pv "${IMAGE_DIR}/rootfs/boot" && 
-   mount -t vfat "/dev/mapper/${MAPPING[0]}" "${IMAGE_DIR}/rootfs/boot"
-check_status
+echo -n "Mounting part2"
+mount "${LOOP}" "${MNT_TMP}"
+echo "creating boot directory"
+mkdir -pv "${MNT_TMP}/boot"
+echo -n "Mounting part1 on ${MNT_TMP}/boot"
+PART1_OFFSET="$((2048 * 512))"
+PART1_SIZELIMIT="$((${SYSTEM_SIZE} * 1024 * 1024))"
+losetup -o "${PART1_OFFSET}" --sizelimit "${PART1_SIZELIMIT}" "${LOOP}" "${DISK}"
+mount -t vfat "${LOOP}" "${MNT_TMP}/boot"
 
 # unpack base packages into image
 echo "Unpacking base packages..."
-"${SCRIPTS}/utils/unpack.sh" "${IMAGE_DIR}/rootfs"
-check_status
-
-echo -n "Syncing disk..."
+"${SCRIPTS}/utils/unpack.sh" "${MNT_TMP}"
 sync
-check_status
 
 ## cleanup ##
 # unmount
 echo -n "Unmounting image..."
-umount "${IMAGE_DIR}/rootfs/boot" && umount "${IMAGE_DIR}/rootfs"
-check_status
+umount "${MNT_TMP}/boot"
+umount "${MNT_TMP}"
 
 # cleanup loopbacks
 echo -n "Cleaning up loopback devices..."
-kpartx -d "${IMAGE_DIR}/${IMAGE_NAME}"
+losetup --detach-all
 check_status
 
 ## Completed ##
