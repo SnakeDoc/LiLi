bb26e8fd6f6010ff801aeeaad9ea76fe7322add0
Jason Sipula <alupis1@gmail.com>
2015-01-28 12:26:10 -0800

    initial nftables support

---
+++ scripts/system/nftables.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# nftables
+
+set -e
+set -u
+
+pkg_dir="$(locate_package 'nftables')"
+
+. "${pkg_dir}/package.mk"
+
+cd "${CLFS_SOURCES}/"
+if [ ! -e "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}.tar.bz2" ]; then
+    wget --read-timeout=20 "${PKG_URL}"
+fi
+
+cd "${CLFS_SOURCES}/"
+# make sure things are clean
+if [ -d "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}" ]; then
+    rm -rf "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}"
+fi
+
+# business time
+cd "${CLFS_SOURCES}/"
+tar -xjvf "${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+
+cd "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}/"
+
+patch -Np1 -i "${SOURCES}/${PKG_NAME}-musl.patch"
+
+CC="${CLFS_ENV_PATH}/${CLFS_TARGET}-gcc" ./configure "${PKG_CONFIGURE_OPTS[@]}"
+  
+CC="${CLFS_ENV_PATH}/${CLFS_TARGET}-gcc" make
+
+make install prefix="${FAKEROOT}"
+
+# cleanup
+cd "${CLFS_SOURCES}/"
+rm -rf "${PKG_NAME}-${PKG_VERSION}"
+
+exit 0
+
