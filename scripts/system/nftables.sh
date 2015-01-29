d6b6510579a81196eb0b9162e61ae00178bd31be
Jason Sipula <alupis1@gmail.com>
2015-01-28 22:05:32 -0800

    Add supporing nftables libs libmnl and libnftnl and build scripts and patches for musl-libc compatibility.

--- scripts/system/nftables.sh
+++ scripts/system/nftables.sh
@@ -5,33 +5,68 @@
 set -e
 set -u
 
-pkg_dir="$(locate_package 'nftables')"
-
-. "${pkg_dir}/package.mk"
-
+# build libmnl
+sub_pkg_dir="$(locate_package 'libmnl')"
+. "${sub_pkg_dir}/package.mk"
 cd "${CLFS_SOURCES}/"
 if [ ! -e "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}.tar.bz2" ]; then
     wget --read-timeout=20 "${PKG_URL}"
 fi
-
 cd "${CLFS_SOURCES}/"
 # make sure things are clean
 if [ -d "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}" ]; then
     rm -rf "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}"
 fi
-
-# business time
 cd "${CLFS_SOURCES}/"
 tar -xjvf "${PKG_NAME}-${PKG_VERSION}.tar.bz2"
-
 cd "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}/"
-
 patch -Np1 -i "${SOURCES}/${PKG_NAME}-musl.patch"
-
 CC="${CLFS_ENV_PATH}/${CLFS_TARGET}-gcc" ./configure "${PKG_CONFIGURE_OPTS[@]}"
-  
 CC="${CLFS_ENV_PATH}/${CLFS_TARGET}-gcc" make
+make install prefix="${FAKEROOT}"
+cd "${CLFS_SOURCES}/"
+rm -rf "${PKG_NAME}-${PKG_VERSION}"
 
+# build libnftnl
+sub_pkg_dir="$(locate_package 'libnftnl')"
+. "${sub_pkg_dir}/package.mk"
+cd "${CLFS_SOURCES}/"
+if [ ! -e "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}.tar.bz2" ]; then
+    wget --read-timeout=20 "${PKG_URL}"
+fi
+cd "${CLFS_SOURCES}/"
+# make sure things are clean
+if [ -d "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}" ]; then
+    rm -rf "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}"
+fi
+cd "${CLFS_SOURCES}/"
+tar -xjvf "${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+cd "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}/"
+patch -Np1 -i "${SOURCES}/${PKG_NAME}-musl.patch"
+CC="${CLFS_ENV_PATH}/${CLFS_TARGET}-gcc" ./configure "${PKG_CONFIGURE_OPTS[@]}"
+CC="${CLFS_ENV_PATH}/${CLFS_TARGET}-gcc" make
+make install prefix="${FAKEROOT}"
+cd "${CLFS_SOURCES}/"
+rm -rf "${PKG_NAME}-${PKG_VERSION}"
+
+# build nftables
+pkg_dir="$(locate_package 'nftables')"
+. "${pkg_dir}/package.mk"
+cd "${CLFS_SOURCES}/"
+if [ ! -e "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}.tar.bz2" ]; then
+    wget --read-timeout=20 "${PKG_URL}"
+fi
+cd "${CLFS_SOURCES}/"
+# make sure things are clean
+if [ -d "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}" ]; then
+    rm -rf "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}"
+fi
+cd "${CLFS_SOURCES}/"
+tar -xjvf "${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+cd "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}/"
+patch -Np1 -i "${SOURCES}/${PKG_NAME}-musl.patch"
+CC="${CLFS_ENV_PATH}/${CLFS_TARGET}-gcc" ./configure "${PKG_CONFIGURE_OPTS[@]}"
+CC="${CLFS_ENV_PATH}/${CLFS_TARGET}-gcc" make
 make install prefix="${FAKEROOT}"
 
 # cleanup
