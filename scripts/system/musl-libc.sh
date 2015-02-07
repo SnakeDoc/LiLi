517d7bb21d6a9b0836ed2bf260fb4f3a37418a5b
Jason Sipula <alupis1@gmail.com>
2015-02-07 14:09:07 -0800

    ensure bin dir is available

--- scripts/system/musl-libc.sh
+++ scripts/system/musl-libc.sh
@@ -44,6 +44,7 @@ sync
 
 # make some sym links for compatibility
 ln -svf "ld-musl-${CLFS_ARCH}hf.so.1" "${FAKEROOT}/lib/ld-musl-${CLFS_ARCH}hf.so"
+mkdir -pv "${FAKEROOT}/bin"
 ln -svf ../lib/ld-musl-"${CLFS_ARCH}"hf.so "${FAKEROOT}/bin/ldd"
 
 # cleanup
