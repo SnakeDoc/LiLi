532b0575d2dcd7a3a45ef88b8249949509a95efb
Jason Sipula <alupis1@gmail.com>
2015-02-20 08:36:03 -0800

    make coreutils install bash config files to profile.d

--- scripts/system/coreutils.sh
+++ scripts/system/coreutils.sh
@@ -57,6 +57,61 @@ mv -v "${FAKEROOT}/usr/bin/"{dd,df,echo,false,ln,ls,mkdir,mknod} "${FAKEROOT}/bi
 mv -v "${FAKEROOT}/usr/bin/"{mv,pwd,rm,rmdir,stty,true,uname} "${FAKEROOT}/bin"
 mv -v "${FAKEROOT}/usr/bin/chroot" "${FAKEROOT}/usr/sbin"
 
+# install some bash config files specific to coreutils
+mkdir -pv "${FAKEROOT}/etc/profile.d"
+
+# /etc/profile.d/colorls.sh
+cat > "${FAKEROOT}/etc/profile.d/colorls.sh" << "EOF"
+# color-ls initialization
+
+#when USER_LS_COLORS defined do not override user LS_COLORS, but use them.
+if [ -z "$USER_LS_COLORS" ]; then
+
+  alias ll='ls -l' 2>/dev/null
+  alias l.='ls -d .*' 2>/dev/null
+
+
+  # Skip the rest for noninteractive shells.
+  [ -z "$PS1" ] && return
+
+  COLORS=
+
+  for colors in "$HOME/.dir_colors.$TERM" "$HOME/.dircolors.$TERM" \
+      "$HOME/.dir_colors" "$HOME/.dircolors"; do
+    [ -e "$colors" ] && COLORS="$colors" && break
+  done
+
+  [ -z "$COLORS" ] && [ -e "/etc/DIR_COLORS.256color" ] && \
+      [ "x`tty -s && tput colors 2>/dev/null`" = "x256" ] && \
+      COLORS="/etc/DIR_COLORS.256color"
+
+  if [ -z "$COLORS" ]; then
+    for colors in "/etc/DIR_COLORS.$TERM" "/etc/DIR_COLORS" ; do
+      [ -e "$colors" ] && COLORS="$colors" && break
+    done
+  fi
+
+  # Existence of $COLORS already checked above.
+  [ -n "$COLORS" ] || return
+
+  eval "`dircolors --sh "$COLORS" 2>/dev/null`"
+  [ -z "$LS_COLORS" ] && return
+  grep -qi "^COLOR.*none" $COLORS >/dev/null 2>/dev/null && return
+fi
+
+alias ll='ls -l --color=auto' 2>/dev/null
+alias l.='ls -d .* --color=auto' 2>/dev/null
+alias ls='ls --color=auto' 2>/dev/null
+EOF
+
+# /etc/profile.d/which2.sh
+cat > "${FAKEROOT}/etc/profile.d/which2.sh" << "EOF"
+# Initialization script for bash and sh
+
+# export AFS if you are in AFS environment
+alias which='alias | /usr/bin/which --tty-only --read-alias --show-dot --show-tilde'
+EOF
+
 # cleanup
 cd "${CLFS_SOURCES}/"
 rm -rf "${PKG_NAME}-${PKG_VERSION}"
